21394c857116088a680cbaafb7d99d97e805104b
tj-10 <1010junpei@gmail.com>
2026-01-12 20:29:59 +0900

    01-23

---
+++ 01.Jan/23/5.ABC_090_B_Palindromic_Numbers/main.cpp
@@ -0,0 +1,23 @@
+# include <iostream>
+# include <string>
+using namespace std;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  int count = 0;
+
+  for (int i = A; i <= B; i++) {
+    string s = to_string(i);
+
+    if (s[0] == s[4] && s[1] == s[3]) {
+      count++;
+    }
+  }
+
+  cout << count << endl;
+  return 0;
+}
+
+

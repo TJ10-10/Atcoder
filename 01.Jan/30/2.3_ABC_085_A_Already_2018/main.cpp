ce81c1ecaf2ed0977e3aef015edc95a2b55dec8d
tj-10 <1010junpei@gmail.com>
2026-01-20 12:38:22 +0900

    01-20

--- 01.Jan/30/2.3_ABC_085_A_Already_2018/main.cpp
+++ 01.Jan/30/2.3_ABC_085_A_Already_2018/main.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
 #include <string>
+using namespace std;
+
+int main() {
+  string S;
+
+  cin >> S;
+
+  S[0] = '2';
+  S[1] = '0';
+  S[2] = '1';
+  S[3] = '8';
+
+  cout << S << endl;
+
+  return 0;
+}

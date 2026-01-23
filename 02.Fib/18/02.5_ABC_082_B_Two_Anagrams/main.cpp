20c5ac6ead1314400732fce8ae158f70082537a2
tj-10 <1010junpei@gmail.com>
2026-01-23 14:22:33 +0900

    01-23

---
+++ 02.Fib/18/02.5_ABC_082_B_Two_Anagrams/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  string s, t;
+  cin >> s >> t;
+
+  sort(s.begin(), s.end());
+
+  sort(t.begin(), t.end(), greater<char>());
+
+  if (s < t) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+
+  return 0;
+}

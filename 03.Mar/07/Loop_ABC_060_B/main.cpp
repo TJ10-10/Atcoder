9769ae39df5f2ab5059d00a3e235bda52270ab4c
tj-10 <1010junpei@gmail.com>
2026-01-26 12:26:30 +0900

    01-26

---
+++ 03.Mar/07/Loop_ABC_060_B/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int A, B, C;
+  cin >> A >> B >> C;
+
+  bool can = false;
+
+  for (int i = 1; i <= B; ++i) {
+    if ((i * A) % B == C) {
+      can = true;
+      break;
+    }
+  }
+
+  if (can) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+
+  return 0;
+}
